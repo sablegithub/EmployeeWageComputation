c2765397a9edcc0115e66539d924f6b6b53a7674
vaibhav sable <vaibhavsable52@gmail.com>
2022-05-04 06:37:56 -0700

    Attendence of Employee

---
+++ EmployeeWage/EmpPresentorAbsent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmpPresentorAbsent
+    {
+        public void Attendance()
+        {
+            int FULL_TIME = 1;
+            Random r=new Random();
+            int check=r.Next(0,2);
+            if(check==1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("welcome to employee wage computation program");
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("welcome to employee wage computation program");
+            EmpPresentorAbsent E = new EmpPresentorAbsent();
+            E.Attendance();
+        }
+    }
+}
