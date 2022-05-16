76ca3479be6002a365e4aadf009353aa943163fe
sablegithub <103979429+sablegithub@users.noreply.github.com>
2022-05-16 07:56:04 -0700

    Update EMPwageUsingClass.cs

--- EmployeeWage/EMPwageUsingClass.cs
+++ EmployeeWage/EMPwageUsingClass.cs
@@ -7,37 +7,43 @@ using System.Threading.Tasks;
 namespace EmployeeWage
 {
 
-    
-    public class EMPwageUsingClass
+    public class Companies
     {
-        const int FULL_TIME = 1;
-        const int EMP_HOUR = 20;
-        const int PART_TIME = 2;
-        int emphour = 0;
-        int empWage = 0;
-
-
-        public void method()
+        public static int Company(String Name, int emp_rate_Per_hour, int No_Working_Days, int Max_hour_Per_Month)
         {
-            Random r = new Random();
-            int check = r.Next(0,3);
-            if(check == FULL_TIME)
-            {
-                emphour = 8;
-                Console.WriteLine("Full Time:"+(empWage=emphour*EMP_HOUR));
-                
+             int Totalemphours = 0;
+             int TotalWorkingdays = 0;
+             int Emphours = 0;
+             int Total_Empwage = 0;
 
-            }
-            else if(check == PART_TIME)
+            while (Totalemphours <= Max_hour_Per_Month && TotalWorkingdays < No_Working_Days)
             {
-                emphour = 4;
-                Console.WriteLine("Part Time :"+(empWage = emphour * EMP_HOUR));
+                TotalWorkingdays++;
+                Random r = new Random();
+                int check = r.Next(0, 3);
+                switch (check)
+                {
+                    case 1:
+                        Emphours = 4;
+                        Console.WriteLine("Employe is Working as Part Time");
+                        break;
+                    case 2:
+                        Emphours = 8;
+                        Console.WriteLine("Employee is Working as Full Time");
+
+                        break;
+                    case 3:
+                        Emphours = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+               Totalemphours += Emphours;
+                Console.WriteLine("Day :" +TotalWorkingdays+  "Emp Hours :" + Emphours);
             }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
-           
+            Total_Empwage = Totalemphours * emp_rate_Per_hour;
+            Console.WriteLine("Total Employee Wage for : " + Name + " is " + Total_Empwage);
+            return Total_Empwage;
         }
+
     }
 }
